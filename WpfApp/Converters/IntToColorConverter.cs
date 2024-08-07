2395a1ed282d17f20c42e08dcb182a71fbabc7f4
Jean-Charles Durand <jc.buchu@gmail.com>
2024-08-07 12:26:24 +0200

    add a converter example

---
+++ WpfApp/Converters/IntToColorConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace WpfApp.Converters
+{
+    public class IntToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int valueAsInt = (int)value;
+
+            if (valueAsInt < 0)
+            {
+                return new SolidColorBrush(Colors.Blue);
+            }
+            else if (valueAsInt < 10)
+            {
+                return new SolidColorBrush(Colors.Chartreuse);
+            }
+            else
+            { 
+                return new SolidColorBrush(Colors.Red); 
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
